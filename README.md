b1433670b38f74d2a09be0e30b7e9d33a2c2e123
david1707 <DavidAlacant@gmail.com>
2021-06-10 15:23:51 +0200

    (-)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 An application where the users can add/remove music albums to/from their collection, share them, whislist, Admin users can also add, modify or delete albums, bands, and more, and Mega also assign who is an Admin or a normal user
 
 Future updates:
-- [WIP]Add login/signup Screens and improve style
+- Add login/signup Screens and improve style
 - Improve AvatarGlow https://pub.dev/packages/avatar_glow
 - Add global styling to the app
 - Add email Auth with Firebase (Email, password, name, type of user [Normal (adds/remove music albums,sharing), Admin (Normal + Add/Modify/Delete albums and bands ), Mega(Admin + add user as Admin/Normal + Create music styles) ])
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:avatar_glow/avatar_glow.dart';
 
-import 'login_screen.dart';
-import 'widgets/start_fade_transition.dart';
+import './login_screen.dart';
+import '../widgets/start_fade_transition.dart';
 
 class SplashScreen extends StatelessWidget {
   static const routeName = '/splash';
@@ -12,69 +12,66 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            AvatarGlow(
-              endRadius: 60.0,
-              glowColor: Colors.blue,
-              child: Material(
-                color: Colors.red,
-                shadowColor: Colors.green,
-                elevation: 8.0,
-                shape: CircleBorder(),
-                child: CircleAvatar(
-                  backgroundColor: Colors.grey[100],
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    height: 50,
-                  ),
-                  radius: 30.0,
-                ),
-              ),
-            ),
-            StartFadeTransition(
-              curves: Curves.easeInOut,
-              durationInMilliseconds: 2000,
-              delay: 1000,
-              child: Text(
-                'Welcome to MyMusic',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            StartFadeTransition(
-              curves: Curves.easeInOut,
-              durationInMilliseconds: 2000,
-              delay: 2000,
-              child: Text(
-                'A place where you \n control the music',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            StartFadeTransition(
-              curves: Curves.easeInOut,
-              durationInMilliseconds: 1000,
-              delay: 3500,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              AvatarGlow(
+                endRadius: 60.0,
+                glowColor: Colors.blue,
+                child: Material(
+                  color: Colors.red,
+                  shadowColor: Colors.green,
+                  elevation: 8.0,
+                  shape: CircleBorder(),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey[100],
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      height: 50,
+                    ),
+                    radius: 30.0,
                   ),
                 ),
-                child: Text('Login'),
-                onPressed: () {
-                  // TODO Check if the login info is SharedPreferences. If it is, navigate to main page. If not, navigate to login
-                  Navigator.of(context).pushNamed(LoginScreen.routeName);
-                },
               ),
-            ),
-          ],
+              StartFadeTransition(
+                curves: Curves.easeInOut,
+                durationInMilliseconds: 2000,
+                delay: 1000,
+                child: Text(
+                  'Welcome to MyMusic',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              StartFadeTransition(
+                curves: Curves.easeInOut,
+                durationInMilliseconds: 2000,
+                delay: 2000,
+                child: Text(
+                  'A place where you \n control the music',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              StartFadeTransition(
+                curves: Curves.easeInOut,
+                durationInMilliseconds: 1000,
+                delay: 3500,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text('Login'),
+                  onPressed: () {
+                    // TODO Check if the login info is SharedPreferences. If it is, navigate to main page. If not, navigate to login
+                    Navigator.of(context).pushNamed(LoginScreen.routeName);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
