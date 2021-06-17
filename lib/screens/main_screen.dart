b259200542dc74612f3f03b9586575cf87eb02f1
david1707 <DavidAlacant@gmail.com>
2021-06-17 13:15:09 +0200

    Added signout on MainScreen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,10 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
-import 'package:badges/badges.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:badges/badges.dart';
+import 'package:my_music/screens/login_screen.dart';
+
 class MainScreen extends StatefulWidget {
   static const routeName = '/';
 
@@ -17,9 +17,6 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-
-    inspect(FirebaseAuth.instance);
-    
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
@@ -35,8 +32,18 @@ class _MainScreenState extends State<MainScreen> {
                 totalWishlist.toString(),
                 style: TextStyle(color: Colors.white),
               ),
-              child: Icon(Icons.favorite, color: Colors.white,),
+              child: Icon(
+                Icons.favorite,
+                color: Colors.white,
+              ),
             ),
+          ),
+          IconButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamed(LoginScreen.routeName);
+            },
+            icon: Icon(Icons.logout),
           ),
         ],
       ),
@@ -45,15 +52,25 @@ class _MainScreenState extends State<MainScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('Welcome YOUR NAME !'),
-            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+            ),
             Text('You have 40 albums in your collection.'),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Text('Click here to view  more albums.'),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Text('More options'),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Text('More options'),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Text('More options'),
           ],
         ),
