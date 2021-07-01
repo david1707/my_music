1373be5f1f18fd5e0fc6eb4177cc5d5c6092f8bf
david1707 <DavidAlacant@gmail.com>
2021-07-01 18:25:34 +0200

    [WIP]GenreList: Moved Dialogs to its own file to clean the Widget

---
+++ lib/models/genre/genre_dialog.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+import './genre.dart';
+import '../../helper/snackbar.dart';
+
+Future showDeleteDialog(BuildContext context, Genre genre) async {
+  return showDialog(
+    context: context,
+    builder: (_) => AlertDialog(
+      title: Text('Do you want to delete ${genre.title}?'),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () async {
+            try {
+              await Genre.deleteGenre(genre.id);
+            } on Exception catch (_) {
+              showSnackBar(
+                text: 'Unknown error updating the genre.',
+                color: Colors.red,
+                context: context,
+              );
+            } finally {
+              Navigator.pop(context);
+            }
+          },
+          child: Text(
+            'Delete',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            'Cancel',
+            style: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Future showEditTitleDialog(
+    BuildContext context,
+    Genre genre,
+    GlobalKey<FormState> _keyDialogForm,
+    TextEditingController _titleController) {
+  return showDialog(
+    context: context,
+    builder: (_) => AlertDialog(
+      title: Form(
+        key: _keyDialogForm,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.edit),
+              ),
+              controller: _titleController,
+              maxLength: 20,
+              textAlign: TextAlign.center,
+              onSaved: (value) {
+                _titleController.text = value;
+              },
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Enter a genre, please.';
+                } else if (value.length < 2) {
+                  return 'Too short.';
+                }
+                return null;
+              },
+            )
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () async {
+            if (_keyDialogForm.currentState.validate()) {
+              _keyDialogForm.currentState.save();
+              try {
+                await Genre.updateGenre(genre.id, _titleController.text);
+              } on Exception catch (_) {
+                showSnackBar(
+                  text: 'Unknown error updating the genre.',
+                  color: Colors.red,
+                  context: context,
+                );
+              } finally {
+                Navigator.pop(context);
+              }
+            }
+          },
+          child: Text(
+            'Update',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            'Cancel',
+            style: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
--- lib/models/genre/genre_list.dart
+++ lib/models/genre/genre_list.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './genre.dart';
+import './genre_dialog.dart';
 import '../../constants.dart';
-import '../../helper/snackbar.dart';
 
 class GenreList extends StatelessWidget {
   final TextEditingController _titleController;
@@ -48,7 +48,12 @@ class GenreList extends StatelessWidget {
                   icon: Icon(Icons.edit),
                   onPressed: () {
                     _titleController.text = genre.title;
-                    showEditTitleDialog(context, genre);
+                    showEditTitleDialog(
+                      context,
+                      genre,
+                      _keyDialogForm,
+                      _titleController,
+                    );
                   },
                 ),
               ],
@@ -57,120 +62,6 @@ class GenreList extends StatelessWidget {
         );
       },
       itemCount: genres.length,
-    );
-  }
-
-  Future showDeleteDialog(BuildContext context, Genre genre) async {
-    return showDialog(
-      context: context,
-      builder: (_) => AlertDialog(
-        title: Text('Do you want to delete ${genre.title}?'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () async {
-              try {
-                await Genre.deleteGenre(genre.id);
-              } on Exception catch (_) {
-                showSnackBar(
-                  text: 'Unknown error updating the genre.',
-                  color: Colors.red,
-                  context: context,
-                );
-              } finally {
-                Navigator.pop(context);
-              }
-            },
-            child: Text(
-              'Delete',
-              style: TextStyle(
-                color: Colors.blue,
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text(
-              'Cancel',
-              style: TextStyle(
-                color: Colors.red,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future showEditTitleDialog(BuildContext context, Genre genre) {
-    return showDialog(
-      context: context,
-      builder: (_) => AlertDialog(
-        title: Form(
-          key: _keyDialogForm,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.edit),
-                ),
-                controller: _titleController,
-                maxLength: 20,
-                textAlign: TextAlign.center,
-                onSaved: (value) {
-                  _titleController.text = value;
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Enter a genre, please.';
-                  } else if (value.length < 2) {
-                    return 'Too short.';
-                  }
-                  return null;
-                },
-              )
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () async {
-              if (_keyDialogForm.currentState.validate()) {
-                _keyDialogForm.currentState.save();
-                try {
-                  await Genre.updateGenre(genre.id, _titleController.text);
-                } on Exception catch (_) {
-                  showSnackBar(
-                    text: 'Unknown error updating the genre.',
-                    color: Colors.red,
-                    context: context,
-                  );
-                } finally {
-                  Navigator.pop(context);
-                }
-              }
-            },
-            child: Text(
-              'Update',
-              style: TextStyle(
-                color: Colors.blue,
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text(
-              'Cancel',
-              style: TextStyle(
-                color: Colors.red,
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
